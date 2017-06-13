260721972cd1ee4ab50950781ebf3c17def3ab73
Viktor Savonin <viktor.savonin@meawallet.com>
2017-06-13 23:39:32 +0300

    refactoring

--- src/lesson2/ForExample.java
+++ src/lesson2/ForExample.java
@@ -1,3 +1,5 @@
+package lesson2;
+
 public class ForExample {
 
     public static void main(String[] args) {
--- src/lesson2/PlusPlus.java
+++ src/lesson2/PlusPlus.java
@@ -1,3 +1,5 @@
+package lesson2;
+
 public class PlusPlus {
 
     public static void main(String[] args) {
