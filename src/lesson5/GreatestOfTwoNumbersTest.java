0a54c59f6d71ddf2703e5312cc8c549f12dd2ee5
Viktor Savonin <viktor.savonin@meawallet.com>
2017-07-05 17:40:05 +0300

    Lesson 5 - fix

--- src/lesson5/GreatestOfTwoNumbersTest.java
+++ src/lesson5/GreatestOfTwoNumbersTest.java
@@ -9,26 +9,26 @@ public class GreatestOfTwoNumbersTest {
     }
 
     private static void test1() {
-        int maxNumber = GreatestOfTwoNumbers.findMaxNumber(1, 2);
+        int maxNumber = CommonMethods.findMaxNumber(1, 2);
         boolean isOk = maxNumber == 2;
         checkResult("TEST 1", isOk);
     }
 
     private static void test2() {
-        int maxNumber = GreatestOfTwoNumbers.findMaxNumber(2, 1);
+        int maxNumber = CommonMethods.findMaxNumber(2, 1);
         checkResult("TEST 2", maxNumber == 2);
     }
 
     private static void test3() {
-        int maxNumber = GreatestOfTwoNumbers.findMaxNumber(2, 2);
+        int maxNumber = CommonMethods.findMaxNumber(2, 2);
         checkResult("TEST 3", maxNumber == 2);
     }
 
     private static void checkResult(String testName, boolean result) {
         if (result) {
-            GreatestOfTwoNumbers.printMessageToConsole(testName + " = OK");
+            CommonMethods.printMessageToConsole(testName + " = OK");
         } else {
-            GreatestOfTwoNumbers.printMessageToConsole(testName + " = FAIL");
+            CommonMethods.printMessageToConsole(testName + " = FAIL");
         }
     }
 
